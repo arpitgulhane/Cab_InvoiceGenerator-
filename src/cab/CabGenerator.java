8a53f85e0fa4a632641a779fc0e184c66f415f64
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-04-12 02:19:11 +0530

    iven a user id , the service gets list of rides

--- src/cab/CabGenerator.java
+++ src/cab/CabGenerator.java
@@ -3,7 +3,6 @@ package cab;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 public class CabGenerator {
     public static final double COST_PER_KM = 10.0;
     public static final double COST_PER_MINUTE = 1.0;
@@ -12,28 +11,17 @@ public class CabGenerator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Ride> rides = new ArrayList<>();
+        System.out.print("Enter user id: ");
+        String userId = scanner.nextLine();
 
-        System.out.print("Enter the number of rides: ");
-        int numRides = scanner.nextInt();
+        RideRepository rideRepository = new RideRepository();
 
-        for (int i = 1; i <= numRides; i++) {
-            System.out.printf("Ride %d:\n", i);
-
-            System.out.print("Enter the distance travelled in km: ");
-            double distance = scanner.nextDouble();
-
-            System.out.print("Enter the time taken in minutes: ");
-            double time = scanner.nextDouble();
-
-            Ride ride = new Ride(distance, time);
-            rides.add(ride);
-        }
+        List<Ride> rides = rideRepository.getRidesByUserId(userId);
 
         double totalFare = calculateTotalFare(rides);
-        double avgFarePerRide = calculateAvgFarePerRide(totalFare, numRides);
+        double avgFarePerRide = calculateAvgFarePerRide(totalFare, rides.size());
 
-        System.out.printf("Total number of rides: %d\n", numRides);
+        System.out.printf("Total number of rides: %d\n", rides.size());
         System.out.printf("Total fare: Rs. %.2f\n", totalFare);
         System.out.printf("Average fare per ride: Rs. %.2f\n", avgFarePerRide);
 
@@ -70,6 +58,23 @@ public class CabGenerator {
         public Ride(double distance, double time) {
             this.distance = distance;
             this.time = time;
+        }
+    }
+
+    private static class RideRepository {
+        private final List<Ride> rides;
+
+        public RideRepository() {
+            rides = new ArrayList<>();
+
+            rides.add(new Ride(10.0, 30.0));
+            rides.add(new Ride(5.0, 15.0));
+            rides.add(new Ride(20.0, 60.0));
+        }
+
+        public List<Ride> getRidesByUserId(String userId) {
+            // In this example, the repository returns a fixed list of rides for any given user ID.
+            return rides;
         }
     }
 }
