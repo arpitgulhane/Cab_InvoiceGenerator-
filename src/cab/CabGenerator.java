e1ad452a71038e19a1e40b57eb4fa2840e5d68b6
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-04-12 02:27:32 +0530

    premium rides(Bonus)

--- src/cab/CabGenerator.java
+++ src/cab/CabGenerator.java
@@ -3,10 +3,18 @@ package cab;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CabGenerator {
-    public static final double COST_PER_KM = 10.0;
-    public static final double COST_PER_MINUTE = 1.0;
-    public static final double MINIMUM_FARE = 5.0;
+    public static final double NORMAL_COST_PER_KM = 10.0;
+    public static final double NORMAL_COST_PER_MINUTE = 1.0;
+    public static final double NORMAL_MINIMUM_FARE = 5.0;
+
+    public static final double PREMIUM_COST_PER_KM = 15.0;
+    public static final double PREMIUM_COST_PER_MINUTE = 2.0;
+    public static final double PREMIUM_MINIMUM_FARE = 20.0;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -18,21 +26,42 @@ public class CabGenerator {
 
         List<Ride> rides = rideRepository.getRidesByUserId(userId);
 
-        double totalFare = calculateTotalFare(rides);
-        double avgFarePerRide = calculateAvgFarePerRide(totalFare, rides.size());
+        double normalTotalFare = calculateTotalFare(rides, RideType.NORMAL);
+        double normalAvgFarePerRide = calculateAvgFarePerRide(normalTotalFare, getNumRidesByType(rides, RideType.NORMAL));
+        System.out.printf("Total number of normal rides: %d\n", getNumRidesByType(rides, RideType.NORMAL));
+        System.out.printf("Total normal fare: Rs. %.2f\n", normalTotalFare);
+        System.out.printf("Average normal fare per ride: Rs. %.2f\n", normalAvgFarePerRide);
 
-        System.out.printf("Total number of rides: %d\n", rides.size());
-        System.out.printf("Total fare: Rs. %.2f\n", totalFare);
-        System.out.printf("Average fare per ride: Rs. %.2f\n", avgFarePerRide);
+        double premiumTotalFare = calculateTotalFare(rides, RideType.PREMIUM);
+        double premiumAvgFarePerRide = calculateAvgFarePerRide(premiumTotalFare, getNumRidesByType(rides, RideType.PREMIUM));
+        System.out.printf("Total number of premium rides: %d\n", getNumRidesByType(rides, RideType.PREMIUM));
+        System.out.printf("Total premium fare: Rs. %.2f\n", premiumTotalFare);
+        System.out.printf("Average premium fare per ride: Rs. %.2f\n", premiumAvgFarePerRide);
 
         scanner.close();
     }
 
-    public static double calculateTotalFare(List<Ride> rides) {
+    public static double calculateTotalFare(List<Ride> rides, RideType rideType) {
+        double costPerKm;
+        double costPerMinute;
+        double minimumFare;
+
+        if (rideType == RideType.NORMAL) {
+            costPerKm = NORMAL_COST_PER_KM;
+            costPerMinute = NORMAL_COST_PER_MINUTE;
+            minimumFare = NORMAL_MINIMUM_FARE;
+        } else {
+            costPerKm = PREMIUM_COST_PER_KM;
+            costPerMinute = PREMIUM_COST_PER_MINUTE;
+            minimumFare = PREMIUM_MINIMUM_FARE;
+        }
+
         double totalFare = 0.0;
 
         for (Ride ride : rides) {
-            totalFare += calculateFare(ride.distance, ride.time);
+            if (ride.type == rideType) {
+                totalFare += calculateFare(ride.distance, ride.time, costPerKm, costPerMinute, minimumFare);
+            }
         }
 
         return totalFare;
@@ -46,36 +75,55 @@ public class CabGenerator {
         }
     }
 
-    public static double calculateFare(double distance, double time) {
-        double fare = distance * COST_PER_KM + time * COST_PER_MINUTE;
-        return Math.max(fare, MINIMUM_FARE);
+    public static double calculateFare(double distance, double time, double costPerKm, double costPerMinute, double minimumFare) {
+        double fare = distance * costPerKm + time * costPerMinute;
+        return Math.max(fare, minimumFare);
     }
 
-    private static class Ride {
-        private final double distance;
-        private final double time;
+    public static int getNumRidesByType(List<Ride> rides, RideType rideType) {
+        int count = 0;
 
-        public Ride(double distance, double time) {
+        for (Ride ride : rides) {
+            if (ride.type == rideType) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public enum RideType {
+        NORMAL, PREMIUM
+    }
+
+    public static class Ride {
+        public final double distance;
+        public final double time;
+        public final RideType type;
+
+        public Ride(double distance, double time, RideType type) {
             this.distance = distance;
             this.time = time;
+            this.type = type;
         }
     }
 
-    private static class RideRepository {
+    public static class RideRepository {
         private final List<Ride> rides;
 
         public RideRepository() {
-            rides = new ArrayList<>();
-
-            rides.add(new Ride(10.0, 30.0));
-            rides.add(new Ride(5.0, 15.0));
-            rides.add(new Ride(20.0, 60.0));
+            this.rides = new ArrayList<>();
+            this.rides.add(new Ride(10.0, 20.0, RideType.NORMAL));
+            this.rides.add(new Ride(5.0, 10.0, RideType.NORMAL));
+            this.rides.add(new Ride(15.0, 30.0, RideType.PREMIUM));
+            this.rides.add(new Ride(20.0, 40.0, RideType.PREMIUM));
+            this.rides.add(new Ride(30.0, 60.0, RideType.PREMIUM));
         }
 
         public List<Ride> getRidesByUserId(String userId) {
-            // In this example, the repository returns a fixed list of rides for any given user ID.
+            // In this example, we only have one user, so we return all the rides.
+            // In a real application, we would look up the rides for the specified user.
             return rides;
         }
     }
 }
-
