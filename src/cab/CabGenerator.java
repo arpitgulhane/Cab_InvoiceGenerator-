b8647d5b878a270367226f2f5997300f059fba35
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-04-11 19:28:08 +0530

    Add multiple ride

--- src/cab/CabGenerator.java
+++ src/cab/CabGenerator.java
@@ -1,14 +1,24 @@
 package cab;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class CabGenerator {
-        public static final double COST_PER_KM = 10.0;
-        public static final double COST_PER_MINUTE = 1.0;
-        public static final double MINIMUM_FARE = 5.0;
+    public static final double COST_PER_KM = 10.0;
+    public static final double COST_PER_MINUTE = 1.0;
+    public static final double MINIMUM_FARE = 5.0;
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Ride> rides = new ArrayList<>();
+
+        System.out.print("Enter the number of rides: ");
+        int numRides = scanner.nextInt();
+
+        for (int i = 1; i <= numRides; i++) {
+            System.out.printf("Ride %d:\n", i);
 
             System.out.print("Enter the distance travelled in km: ");
             double distance = scanner.nextDouble();
@@ -16,15 +26,38 @@ public class CabGenerator {
             System.out.print("Enter the time taken in minutes: ");
             double time = scanner.nextDouble();
 
-            double fare = calculateFare(distance, time);
-            System.out.printf("Total fare: Rs. %.2f\n", fare);
-
-            scanner.close();
+            Ride ride = new Ride(distance, time);
+            rides.add(ride);
         }
 
-        public static double calculateFare(double distance, double time) {
-            double fare = distance * COST_PER_KM + time * COST_PER_MINUTE;
-            return Math.max(fare, MINIMUM_FARE);
+        double totalFare = calculateTotalFare(rides);
+        System.out.printf("Total fare for %d rides: Rs. %.2f\n", numRides, totalFare);
+
+        scanner.close();
+    }
+
+    public static double calculateTotalFare(List<Ride> rides) {
+        double totalFare = 0.0;
+
+        for (Ride ride : rides) {
+            totalFare += calculateFare(ride.distance, ride.time);
         }
 
+        return totalFare;
+    }
+
+    public static double calculateFare(double distance, double time) {
+        double fare = distance * COST_PER_KM + time * COST_PER_MINUTE;
+        return Math.max(fare, MINIMUM_FARE);
+    }
+
+    private static class Ride {
+        private final double distance;
+        private final double time;
+
+        public Ride(double distance, double time) {
+            this.distance = distance;
+            this.time = time;
+        }
+    }
 }
